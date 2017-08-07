4fb29ebec30891b9858d19544c9afd09d37322ee
Fergus <strfer001@myuct.ac.za>
2017-08-07 15:07:41 +0200

    Added test

---
+++ tests/PilotLive/Models/VendorPaymentModelTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fergus
+ * Date: 2017/08/07
+ * Time: 3:06 PM
+ */
+
+namespace DarrynTen\PilotLive\Tests\PilotLive\Models;
+
+use DarrynTen\PilotLive\Models\VendorPayment;
+
+class VendorPaymentModelTest extends BaseModelTest
+{
+    public function testAttributes()
+    {
+        $this->verifyAttributes(VendorPayment::class, [
+            'discountAmount' => [
+                'type' => 'double',
+                'nullable' => false,
+                'readonly' => false,
+            ],
+            'paymentAmount' => [
+                'type' => 'double',
+                'nullable' => false,
+                'readonly' => false,
+            ],
+            'paymentDate' => [
+                'type' => 'string',
+                'nullable' => true,
+                'readonly' => false,
+            ],
+            'paymentReference' => [
+                'type' => 'string',
+                'nullable' => true,
+                'readonly' => false,
+            ],
+            'pilotReference' => [
+                'type' => 'string',
+                'nullable' => true,
+                'readonly' => false,
+            ],
+            'processed' => [
+                'type' => 'boolean',
+                'nullable' => false,
+                'readonly' => false,
+            ],
+            'siteID' => [
+                'type' => 'string',
+                'nullable' => true,
+                'readonly' => false,
+            ],
+            'tipAmount' => [
+                'type' => 'double',
+                'nullable' => false,
+                'readonly' => false,
+            ],
+            'vendorID' => [
+                'type' => 'integer',
+                'nullable' => false,
+                'readonly' => false,
+            ],
+            'vendorPaymentID' => [
+                'type' => 'integer',
+                'nullable' => false,
+                'readonly' => false,
+            ],
+            'voucherAmount' => [
+                'type' => 'double',
+                'nullable' => false,
+                'readonly' => false,
+            ],
+        ]);
+    }
+}
