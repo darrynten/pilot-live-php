bd4de27c44835ac11c6296193cdd78712b3d12fa
Fergus <strfer001@myuct.ac.za>
2017-08-21 14:25:57 +0200

    Test update

--- tests/PilotLive/Models/ItemModelTest.php
+++ tests/PilotLive/Models/ItemModelTest.php
@@ -10,35 +10,45 @@ class ItemModelTest extends BaseModelTest
     {
         $this->verifyAttributes(Item::class, [
             'itemName' => [
-                'type' => 'string',
-                'nullable' => true,
-                'readonly' => false,
-            ],
-            'plu' => [
-                'type' => 'string',
-                'nullable' => true,
-                'readonly' => false,
-            ],
-            'price' => [
-                'type' => 'double',
-                'nullable' => false,
-                'readonly' => false,
-            ],
-            'qty' => [
-                'type' => 'double',
-                'nullable' => false,
-                'readonly' => false,
-            ],
-            'tax' => [
-                'type' => 'double',
-                'nullable' => false,
-                'readonly' => false,
-            ],
-            'value' => [
-                'type' => 'double',
-                'nullable' => false,
-                'readonly' => false,
-            ],
+                'itemName' => [
+                    'type' => 'string',
+                    'nullable' => true,
+                    'readonly' => false,
+                ],
+                'plu' => [
+                    'type' => 'string',
+                    'nullable' => true,
+                    'readonly' => false,
+                ],
+                'price' => [
+                    'type' => 'double',
+                    'nullable' => false,
+                    'readonly' => false,
+                    'min' => 0.00,
+                    'max' => 1000000.00,
+                ],
+                'qty' => [
+                    'type' => 'double',
+                    'nullable' => false,
+                    'readonly' => false,
+                    'min' => 0.00,
+                    'max' => 1000000.00,
+                ],
+                'tax' => [
+                    'type' => 'double',
+                    'nullable' => false,
+                    'readonly' => false,
+                    'min' => 0.00,
+                    'max' => 1000000.00,
+                ],
+                'value' => [
+                    'type' => 'double',
+                    'nullable' => false,
+                    'readonly' => false,
+                    'min' => 0.00,
+                    'max' => 1000000.00,
+                ],
+            ]
         ]);
     }
 }
